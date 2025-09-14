bc42dbd663e2a0fec7dcc39b9ce48c56b5818187
amitsengupta <amitsengupta332@gmail.com>
2025-09-15 00:44:13 +0600

    1015

---
+++ Uri Online Judge/1015.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    float dist = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    printf("%.4f\n", dist);
+
+    return 0;
+}

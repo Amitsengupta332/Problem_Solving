a2c6a4904d17f78c49bbd3bd59931f61c2036e06
amitsengupta <amitsengupta332@gmail.com>
2025-09-15 00:56:02 +0600

    1016_distance.cpp

---
+++ Uri Online Judge/1016_distance.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int dist;
+    cin >> dist;
+    int total = dist * 2;
+    cout << total << " minutos" << endl;
+    return 0;
+}

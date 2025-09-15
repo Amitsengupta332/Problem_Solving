5c5196af5cfb24d93fe583ca0aed0920699ab0a4
amitsengupta <amitsengupta332@gmail.com>
2025-09-15 16:16:10 +0600

    1019

---
+++ Uri Online Judge/1019.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int sec, h, m, s;
+    cin >> sec;
+    h = sec / 3600;
+    m = (sec % 3600) / 60;
+    s = (sec % 3600) % 60;
+    cout << h << ":" << m << ":" << s << endl;
+    return 0;
+}
